cf0f5d1064d8e4f0656a9dbbefc9c45a901cc611
Ayan Masood <ayan.m.dev@gmail.com>
2023-03-28 13:29:04 +0530

    Npm install

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let request = require('request')
 
+
 // Call the Infura API and check that the address is valid.
+let options = {
+    url: 'https://filecoin.infura.io',
+    method: 'post',
+    headers: {
+       'content-type': 'application/json'
+    },
+    auth: {
+      user: '2NZyKqFdKMxQnqxvyNObj4jPrcC',
+      pass: '9f799f9942188d380becd74014b45e04'
+    },
+    body: `{
+      "jsonrpc": "2.0",
+      "id": 0,
+      "method": "Filecoin.ChainHead"
+    }`
+}
 
 request(options, (error, response, body) => {
  if (error) {
